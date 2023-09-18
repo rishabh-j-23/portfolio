381c60b202831aebea0e9e6a1ceb297412857db2
rishabh-j-23 <rjain2311200@gmail.com>
2023-09-18 23:05:55 +0530

    Revert "minor adds"
    This reverts commit ed101c8d5a61b82fd8ebf43451235f1ba7caa109.

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,8 +1,5 @@
 var navBar = `
-    <div class="logo">
-        Portfolio
-        <button id="white-mode-toggle">White Mode</button>
-    </div>
+    <div class="logo">Portfolio</div>
     <div class="navigation">
         <a href="/index.html" ><span>Home</span></a> |
         <a href="/projects.html" ><span>Projects</span></a> |
